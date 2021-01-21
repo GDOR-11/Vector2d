16bc81a1b46d2509ef4fea0c44854e7622039c7c
GDOR-11 <77753977+GDOR-11@users.noreply.github.com>
2021-01-21 14:49:47 -0300

    Update Vector.js

--- Vector.js
+++ Vector.js
@@ -2,11 +2,14 @@ const CARTESIAN = 'CARTESIAN';
 const POLAR = 'POLAR';
 class COORDINATE {
 	static #mode = CARTESIAN
+	constructor() {
+		throw TypeError('COORDINATE is not a constructor');
+	}
 	static get MODE() {
 		return COORDINATE.#mode;
 	}
 	static set MODE(mode) {
-		if(mode != CARTESIAN && mode != POLAR) {
+		if(mode !== CARTESIAN && mode !== POLAR) {
 			throw Error('COORDINATE.MODE must be CARTESIAN or POLAR');
 		}
 		COORDINATE.#mode = mode;
@@ -19,25 +22,25 @@ class Vector {
 	#angle = 0;
 	constructor(x, y) {
 		var error;
-		if(COORDINATE.MODE == CARTESIAN) {
+		if(COORDINATE.MODE === CARTESIAN) {
 			this.x = x;
 			this.y = y;
-			if(Number(this.y) != this.y) {
+			if(Number(this.y) !== this.y) {
 				error = 'parameter 2 is not a number';
 				this.y = 0;
 			}
-			if(Number(this.x) != this.x) {
+			if(Number(this.x) !== this.x) {
 				error = 'parameter 1 is not a number';
 				this.x = 0;
 			}
 		} else {
 			this.length = x;
 			this.angle = y;
-			if(Number(this.angle) != this.angle) {
+			if(Number(this.angle) !== this.angle) {
 				error = 'parameter 2 is not a number';
 				this.angle = 0;
 			}
-			if(Number(this.length) != this.length) {
+			if(Number(this.length) !== this.length) {
 				error = 'parameter 1 is not a number';
 				this.length = 0;
 			}
@@ -48,7 +51,7 @@ class Vector {
 	}
 	static isVector(vector) {
 		try {
-			return vector.constructor.name == 'Vector';
+			return vector.constructor.name === 'Vector';
 		} catch(error) {
 			return false;
 		}
@@ -68,19 +71,19 @@ class Vector {
 		this.y = this.y + vector.y;
 	}
 	addAngle(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.angle = this.angle + value;
 	}
 	addLength(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.length = this.length + value;
 	}
 	addScalar(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.x = this.x + value;
@@ -112,10 +115,10 @@ class Vector {
 		}
 	}
 	clampAngle(min, max) {
-		if(Number(min) != min) {
+		if(Number(min) !== min) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(max) != max) {
+		if(Number(max) !== max) {
 			throw Error('parameter 2 is not a number');
 		}
 		if(min < max) {
@@ -125,10 +128,10 @@ class Vector {
 		}
 	}
 	clampLength(min, max) {
-		if(Number(min) != min) {
+		if(Number(min) !== min) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(max) != max) {
+		if(Number(max) !== max) {
 			throw Error('parameter 2 is not a number');
 		}
 		if(min < max) {
@@ -138,10 +141,10 @@ class Vector {
 		}
 	}
 	clampScalar(min, max) {
-		if(Number(min) != min) {
+		if(Number(min) !== min) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(max) != max) {
+		if(Number(max) !== max) {
 			throw Error('parameter 2 is not a number');
 		}
 		if(min < max) {
@@ -190,32 +193,32 @@ class Vector {
 		if(!Vector.isVector(vector)) {
 			throw Error('parameter is not a Vector');
 		}
-		if(vector.x == 0) {
+		if(vector.x === 0) {
 			throw Error('x coordinate of parameter is 0');
 		}
-		if(vector.y == 0) {
+		if(vector.y === 0) {
 			throw Error('y coordinate of parameter is 0');
 		}
 		this.x = this.x / vector.x;
 		this.y = this.y / vector.y;
 	}
 	divideAngle(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.angle = this.angle / value;
 	}
 	divideLength(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.length = this.length / value;
 	}
 	divideScalar(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
-		if(value == 0) {
+		if(value === 0) {
 			throw Error('parameter is 0');
 		}
 		this.x = this.x / value;
@@ -237,7 +240,7 @@ class Vector {
 		if(!Vector.isVector(vector)) {
 			throw Error('parameter is not a Vector');
 		}
-		return this.x == vector.x && this.y == vector.y;
+		return this.x === vector.x && this.y === vector.y;
 	}
 	floor() {
 		this.x = Math.floor(this.x);
@@ -253,7 +256,7 @@ class Vector {
 		if(!Vector.isVector(vector)) {
 			throw Error('parameter 1 is not a Vector');
 		}
-		if(Number(ammount) != ammount) {
+		if(Number(ammount) !== ammount) {
 			throw Error('parameter 2 is not a number');
 		}
 		this.x = this.x * (1 - ammount) + vector.x * ammount;
@@ -266,50 +269,50 @@ class Vector {
 		if(!Vector.isVector(vector2)) {
 			throw Error('parameter 2 is not a Vector');
 		}
-		if(Number(ammount) != ammount) {
+		if(Number(ammount) !== ammount) {
 			throw Error('parameter 3 is not a number');
 		}
 		this.x = vector1.x * (1 - ammount) + vector2.x * ammount;
 		this.y = vector1.y * (1 - ammount) + vector2.y * ammount;
 	}
 	lerpAngle(angle, ammount) {
-		if(Number(angle) != angle) {
+		if(Number(angle) !== angle) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(ammount) != ammount) {
+		if(Number(ammount) !== ammount) {
 			throw Error('parameter 2 is not a number');
 		}
 		this.angle = this.angle * (1 - ammount) + angle * ammount;
 	}
 	lerpBeetwenAngles(angle1, angle2, ammount) {
-		if(Number(angle1) != angle1) {
+		if(Number(angle1) !== angle1) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(angle2) != angle2) {
+		if(Number(angle2) !== angle2) {
 			throw Error('parameter 2 is not a number');
 		}
-		if(Number(ammount) != ammount) {
+		if(Number(ammount) !== ammount) {
 			throw Error('parameter 3 is not a number');
 		}
 		this.angle = angle1 * (1 - ammount) + angle2 * ammount;
 	}
 	lerpLength(length, ammount) {
-		if(Number(length) != length) {
+		if(Number(length) !== length) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(ammount) != ammount) {
+		if(Number(ammount) !== ammount) {
 			throw Error('parameter 2 is not a number');
 		}
 		this.length = this.length * (1 - ammount) + length * ammount;
 	}
 	lerpBeetwenLengths(length1, length2, ammount) {
-		if(Number(length1) != length1) {
+		if(Number(length1) !== length1) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(length2) != length2) {
+		if(Number(length2) !== length2) {
 			throw Error('parameter 2 is not a number');
 		}
-		if(Number(ammount) != ammount) {
+		if(Number(ammount) !== ammount) {
 			throw Error('parameter 3 is not a number');
 		}
 		this.length = length1 * (1 - ammount) + length2 * ammount;
@@ -331,19 +334,19 @@ class Vector {
 		this.y = this.y * vector.y;
 	}
 	multiplyLength(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.length = this.length * value;
 	}
 	multiplyAngle(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.angle = this.angle * value;
 	}
 	multiplyScalar(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.x = this.x * value;
@@ -353,7 +356,7 @@ class Vector {
 		if(!Vector.isVector(center)) {
 			throw Error('parameter 1 is not a Vector');
 		}
-		if(Number(angle) != angle) {
+		if(Number(angle) !== angle) {
 			throw Error('parameter 2 is not a number');
 		}
 		this.x = this.x - center.x;
@@ -433,17 +436,17 @@ class Vector {
 		}
 	}
 	set(x, y) {
-		if(Number(x) != x) {
+		if(Number(x) !== x) {
 			throw Error('parameter 1 is not a number');
 		}
-		if(Number(y) != y) {
+		if(Number(y) !== y) {
 			throw Error('parameter 2 is not a number');
 		}
 		this.x = x;
 		this.y = y;
 	}
 	setScalar(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.x = value;
@@ -461,19 +464,19 @@ class Vector {
 		this.y = this.y - vector.y;
 	}
 	subAngle(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.angle = this.angle - value;
 	}
 	subLength(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.length = this.length - value;
 	}
 	subScalar(value) {
-		if(Number(value) != value) {
+		if(Number(value) !== value) {
 			throw Error('parameter is not a number');
 		}
 		this.x = this.x - value;
@@ -501,7 +504,7 @@ class Vector {
 	set x(x) {
 		this.#x = x;
 		this.#length = Math.hypot(this.x, this.y);
-		this.#angle = this.length == 0 ? undefined : this.y < 0 ? -Math.acos(this.x / this.length) : Math.acos(this.x / this.length);
+		this.#angle = this.length === 0 ? undefined : this.y < 0 ? -Math.acos(this.x / this.length) : Math.acos(this.x / this.length);
 	}
 	get y() {
 		return this.#y;
@@ -509,6 +512,6 @@ class Vector {
 	set y(y) {
 		this.#y = y;
 		this.#length = Math.hypot(this.x, this.y);
-		this.#angle = this.length == 0 ? undefined : this.y < 0 ? -Math.acos(this.x / this.length) : Math.acos(this.x / this.length);
+		this.#angle = this.length === 0 ? undefined : this.y < 0 ? -Math.acos(this.x / this.length) : Math.acos(this.x / this.length);
 	}
 }
